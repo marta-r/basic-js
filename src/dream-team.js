521963730bf67b63b4cb40256b85b2af63c0e760
Marta Rudkouskaya <lvlartik@gmail.com>
2020-03-04 19:22:00 +0300

    feat: add solution for Dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,23 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(a) {
+    if (Array.isArray(a)) {
+        
+        let newArr = a.map(function(item) {
+            if (typeof item == "string") {
+                return item;
+            } else return "0";
+        });
+
+        let newArr2 = newArr.map(item => (item = item.replace(/\s/g, "")));
+
+        let newArr3 = newArr2.map(function(item) {
+            if (/^[a-z]/i.test(item)) {
+                return item[0].toUpperCase();
+            } else return "";
+        });
+
+        newArr3.sort();
+
+        return newArr3.reduce((sum, current) => sum + current, "");
+
+    } else return false;
 };
